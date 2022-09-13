3d000e66a064ddd92668f66ead68b10227101655
Premdeep Singh <premdeepsingh143@gmail.com>
2022-09-13 03:55:07 -0400

    handlesearchsubmit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
 import Search from './components/Search';
 
+const handleSearchSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target[0].value)
+}
+
 function App() {
   return (
     <div>
       <Header title="Images Gallery"/>
-      <Search />
+      <Search handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
                 <Form.Control placeholder="Search for new image..." />
