9728c1376bc7a4e927784a760149ff38d7d6811d
prem36 <103289300+prem36@users.noreply.github.com>
2022-10-09 00:27:42 -0400

    adjust vs code tabbing

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -9,15 +9,17 @@ const Search = ({ word, setWord, handleSubmit }) => {
           <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control 
-                    type="text"
-                    value={word}
-                    onChange={(e) => setWord(e.target.value)}
-                    placeholder="Search for new image..." 
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new image..."
                 />
               </Col>
               <Col>
-                <Button variant="primary" type="submit">Search</Button>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
               </Col>
             </Row>
           </Form>
